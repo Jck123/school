7694d4fe36631ff3c45d69443f939149555ff54b
Jck123 <jck.1.2.3.a@gmail.com>
2020-01-29 13:40:22 -0500

    Reorganized files by lab and program Added lab 2

---
+++ Lab2/waketechinn/waketechinn.cpp
@@ -0,0 +1,11 @@
+//////////////////////////////////////////////////////////////////////////
+// Filename: waketechinn.cpp                                            //
+// Date: January 29, 2020                                               //
+// Programmer: James Kelly                                              //
+//                                                                      //
+// Description:                                                         //
+//      This program asks the guest to enter which hotel room they      //
+//      wish to stay in, then the program ask the guest which meal      // 
+//      combos they would like                                          //
+//////////////////////////////////////////////////////////////////////////
+
