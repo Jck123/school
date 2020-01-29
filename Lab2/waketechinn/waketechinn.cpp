78588d34a28cc907ea0f26a1887356ce98fe1224
Jck123 <jck.1.2.3.a@gmail.com>
2020-01-29 14:08:10 -0500

    Added introduction text

--- Lab2/waketechinn/waketechinn.cpp
+++ Lab2/waketechinn/waketechinn.cpp
@@ -9,3 +9,16 @@
 //      combos they would like                                          //
 //////////////////////////////////////////////////////////////////////////
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int roomNum;
+
+    cout << "Welcome to the Wake Tech Inn! May I take your coat?\nWelcome to the Wake Tech Inn! And while you slumber, you will feel as if you're gently rocking on a boat.\nWhich room would you like during your stay?" << endl;
+    cout << "1      1 Queen Size Bed, Accessible\n2      1 Queen Size Bed, Sofa\n3      1 King Size Bed, Accessible\n4      1 Kind Size Bed, Sofa" << endl;
+    cin >> roomNum;
+    //cout << roomNum;
+    //cin >> roomNum;
+}
