b5042b585bad48446bdef3b5a94785d04671c848
Jck123 <jck.1.2.3.a@gmail.com>
2020-01-31 13:13:49 -0500

    Completed restaurant function

--- Lab2/waketechinn/waketechinn.cpp
+++ Lab2/waketechinn/waketechinn.cpp
@@ -12,17 +12,54 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
 using namespace std;
+
+int roomNum;
+char comboChar;
+double orderPrice = 0.00;
+int orderTotal = 0;
 
 char restaurant()
 {
-    
+    cout << "Enter the letter of the combo you would like to order, or type \'T\' to calculate the total: ";
+    cin >> comboChar;
+
+    switch (toupper(comboChar))
+    {
+    case 'A':
+        orderPrice = orderPrice + 10.25;
+        orderTotal++;
+        restaurant();
+        break;
+    case 'B':
+        orderPrice = orderPrice + 15.75;
+        orderTotal++;
+        restaurant();
+        break;
+    case 'C':
+        orderPrice = orderPrice + 13.25;
+        orderTotal++;
+        restaurant();
+        break;
+    case 'D':
+        orderPrice = orderPrice + 9.75;
+        orderTotal++;
+        restaurant();
+        break;
+    case 'T':
+        cout << "You have ordered " << orderTotal << " items and your total is: $" << setprecision(4) << orderPrice * 1.06 << endl;
+        break;
+    default:
+        cout << "I'm sorry, that is not a valid combo, please try again" << endl;
+        restaurant();
+        break;
+    }
 }
 
 int main()
 {
     srand(time(NULL));
-    int roomNum;
 
     cout << "Welcome to the Wake Tech Inn! May I take your coat?\nWelcome to the Wake Tech Inn! And while you slumber, you will feel as if you're gently rocking on a boat.\nWhich room would you like during your stay?" << endl;
     cout << "1      1 Queen Size Bed, Accessible\n2      1 Queen Size Bed, Sofa\n3      1 King Size Bed, Accessible\n4      1 Kind Size Bed, Sofa" << endl;
@@ -46,5 +83,7 @@ int main()
         main();
         break;
     }
+    cout << "Welcome to the Wake Tech Inn Restaurant! On today's menu, we have\nCombo      Description                                 Price\nCombo A    Curry Chicken with Cabbage and a Drink      $10.25\nCombo B    Tower Special Samosa with a Drink           $15.75\nCombo C    Enpanadas Cubanas w/Shrimp and a Drink      $13.25\nCombo D    Cheeseburgers with Fries and a Drink        $9.75" << endl;
+    restaurant();
     cin >> roomNum;
 }
