e13fee6e815b2cf6a568385bfc83d850d743ec84
Jck123 <jck.1.2.3.a@gmail.com>
2020-02-05 14:49:35 -0500

    Removed global variables and used reference vari

--- Lab2/waketechinn/waketechinn.cpp
+++ Lab2/waketechinn/waketechinn.cpp
@@ -15,13 +15,9 @@
 #include <iomanip>      //For setprecision()
 using namespace std;
 
-int roomNum;
-char comboChar;
-double orderPrice = 0.00;
-int orderTotal = 0;
-
-char restaurant()
+char restaurant(double &orderPrice, int &orderTotal)
 {
+    char comboChar;
     cout << "Enter the letter of the combo you would like to order, or type \'T\' to calculate the total: ";
     cin >> comboChar;
 
@@ -30,35 +26,39 @@ char restaurant()
     case 'A':                               //Combo A
         orderPrice = orderPrice + 10.25;
         orderTotal++;
-        restaurant(); //Reiteration
+        restaurant(orderPrice, orderTotal); //Reiteration
         break;
     case 'B':                               //Combo B
         orderPrice = orderPrice + 15.75;
         orderTotal++;
-        restaurant();
+        restaurant(orderPrice, orderTotal);
         break;
     case 'C':                               //Combo C
         orderPrice = orderPrice + 13.25;
         orderTotal++;
-        restaurant();
+        restaurant(orderPrice, orderTotal);
         break;
     case 'D':                               //Combo D
         orderPrice = orderPrice + 9.75;
         orderTotal++;
-        restaurant();
+        restaurant(orderPrice, orderTotal);
         break;
     case 'T':                               //Calculates total with 6% sales tax
         cout << "You have ordered " << orderTotal << " items and your total is: $" << setprecision(4) << orderPrice * 1.06 << endl;
         break;
     default:                                //Input error
         cout << "I'm sorry, that is not a valid combo, please try again" << endl;
-        restaurant();
+        restaurant(orderPrice, orderTotal);
         break;
     }
 }
 
 int main()
 {
+    double orderPrice = 0.00;
+    int orderTotal = 0;
+    int roomNum;
+
     srand(time(NULL));  //Makes the room number ACTUALLY random
 
     cout << "Welcome to the Wake Tech Inn! May I take your coat?\nWelcome to the Wake Tech Inn! And while you slumber, you will feel as if you're gently rocking on a boat.\nWhich room would you like during your stay?" << endl;
@@ -84,6 +84,6 @@ int main()
         break;
     }
     cout << "Welcome to the Wake Tech Inn Restaurant! On today's menu, we have\nCombo      Description                                 Price\nCombo A    Curry Chicken with Cabbage and a Drink      $10.25\nCombo B    Tower Special Samosa with a Drink           $15.75\nCombo C    Enpanadas Cubanas w/Shrimp and a Drink      $13.25\nCombo D    Cheeseburgers with Fries and a Drink        $9.75" << endl;
-    restaurant();   //Activates the restaurant part of the program
+    restaurant(orderPrice, orderTotal);   //Activates the restaurant part of the program
     cin >> roomNum; //Allows user to read output of program before program closes
 }
