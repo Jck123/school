2e8ecfbdc6736e8fc1b461c9d5019de573fa19af
Jck123 <jck.1.2.3.a@gmail.com>
2020-02-19 13:29:27 -0500

    Added directories for new lab, started first file

---
+++ Lab3/sportsanalytics/sportsanalytics.cpp
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////////////
+// Filename: sportsanalytics.cpp                                        //
+// Date: Februrary 19, 2020                                             //
+// Programmer: James Kelly                                              //
+//                                                                      //
+// Description:                                                         //
+//      This program asks the guest to enter which hotel room they      //
+//      wish to stay in, then the program ask the guest which meal      // 
+//      combos they would like                                          //
+//////////////////////////////////////////////////////////////////////////
+
+int getNumberOfTwoPointsMade()
+{
+
+}
+
+int getNumberOfThreePointsMade()
+{
+
+}
+
+int getTotalNumberOfShotsAttempted()
+{
+
+}
+
+double getPercentageOfTwoPointsMade()
+{
+
+}
+
+double getPercentageOfThreePointsMade()
+{
+
+}
+
+bool IsBetterThanChance()
+{
+
+}
+
+void main()
+{
+    
+}
