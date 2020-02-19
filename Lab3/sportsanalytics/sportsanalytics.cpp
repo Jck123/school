2f7a6073bf2717ef4cc4bf94cb318440c8e8ac51
Jck123 <jck.1.2.3.a@gmail.com>
2020-02-19 14:44:26 -0500

    Completed first function

--- Lab3/sportsanalytics/sportsanalytics.cpp
+++ Lab3/sportsanalytics/sportsanalytics.cpp
@@ -9,37 +9,66 @@
 //      combos they would like                                          //
 //////////////////////////////////////////////////////////////////////////
 
-int getNumberOfTwoPointsMade()
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int getNumberOfTwoPointsMade(string &fileName, ifstream &file)
+{
+    string stat;
+    int num;
+    int ThPM = 0;
+    int FGM = 0;
+
+    file.open(fileName);
+    while(file >> stat >> num)
+    {
+        if (stat == "FGM") {FGM = num;}
+        else if (stat == "3PM") {ThPM = num;}
+    }
+    if (ThPM == 0 || FGM == 0) {return 0;}
+    else return FGM - ThPM;
+}
+
+int getNumberOfThreePointsMade(string &fileName, ifstream &file)
 {
 
 }
 
-int getNumberOfThreePointsMade()
+int getTotalNumberOfShotsAttempted(string &fileName, ifstream &file)
 {
 
 }
 
-int getTotalNumberOfShotsAttempted()
+double getPercentageOfTwoPointsMade(string &fileName, ifstream &file)
 {
 
 }
 
-double getPercentageOfTwoPointsMade()
+double getPercentageOfThreePointsMade(string &fileName, ifstream &file)
 {
 
 }
 
-double getPercentageOfThreePointsMade()
+bool IsBetterThanChance(string &fileName, ifstream &file)
 {
 
 }
 
-bool IsBetterThanChance()
+int main()
 {
-
-}
-
-void main()
-{
+    string fileName;
+    ifstream file;
     
+    cout << "Welcome to the LeBron James Museum, please enter the file you wish to analyze: ";
+    cin >> fileName;
+    
+    cout << getNumberOfTwoPointsMade(fileName, file);
+    getNumberOfThreePointsMade(fileName, file);
+    getTotalNumberOfShotsAttempted(fileName, file);
+    getPercentageOfTwoPointsMade(fileName, file);
+    getPercentageOfThreePointsMade(fileName, file);
+    IsBetterThanChance(fileName, file);
+
+    cin >> fileName;
 }
