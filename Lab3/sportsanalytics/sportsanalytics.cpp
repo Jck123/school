46bb5ed498969b5c62aa003fadc6d2181af60140
Jck123 <jck.1.2.3.a@gmail.com>
2020-02-22 20:59:33 -0500

    Completed sportsanalytics.cpp

--- Lab3/sportsanalytics/sportsanalytics.cpp
+++ Lab3/sportsanalytics/sportsanalytics.cpp
@@ -13,74 +13,68 @@
 #include <fstream>
 using namespace std;
 
-int getNumberOfTwoPointsMade(string &fileName, ifstream &file)
+int getNumberOfTwoPointsMade(int &ThPM, int &FGM)
 {
-    string stat;
-    int num;
-    int ThPM = 0;
-    int FGM = 0;
-
-    file.open(fileName);
-    while(file >> stat >> num)
-    {
-        if (stat == "FGM") {FGM = num;}
-        else if (stat == "3PM") {ThPM = num;}
-    }
-    if (ThPM == 0 || FGM == 0) {return 0;}
-    else return FGM - ThPM;
+    return FGM - ThPM;                  //Subtracts 3 pointers made from total shots made to get 2 pointers made
 }
 
-int getNumberOfThreePointsMade(string &fileName, ifstream &file)
+int getNumberOfThreePointsMade(int &ThPM)
 {
-    string stat;
-    int num = 0;
-
-    file.open(fileName);
-    while(file >> stat >> num)
-    {
-        if (stat == "3PM") {return num;}
-    }
+    return ThPM;                        //Returns three pointers made
 }
 
-int getTotalNumberOfShotsAttempted(string &fileName, ifstream &file)
+int getTotalNumberOfShotsAttempted(int &FGM)
 {
-    string stat;
-    int num;
-
-    file.open(fileName);
-    while(file >> stat >> num)
-    {}
+    return FGM;                         //Returns all attempts for the basket
 }
 
-double getPercentageOfTwoPointsMade(string &fileName, ifstream &file)
+double getPercentageOfTwoPointsMade(double TPM, double ThPA, double FGA)
 {
-
+    return (TPM / (FGA - ThPA)) * 100;  //Subtracts three pointer attempts from all attempts and divides two pointers made by that
 }
 
-double getPercentageOfThreePointsMade(string &fileName, ifstream &file)
+double getPercentageOfThreePointsMade(double ThPM, double ThPA)
 {
-
+    return (ThPM / ThPA) * 100;         //Returns percentage of three pointers made
 }
 
-bool IsBetterThanChance(string &fileName, ifstream &file)
+bool IsBetterThanChance(double FGM, double FGA)
 {
-
+    if (FGM/FGA >= 0.5) {return true;}  //Returns bool value if total shots are 50% or over
+    else {return false;}
 }
 
 int main()
 {
-    string fileName;        //You're doing it wrong, get variables from files first, then pass them to the functions
+    string fileName;
     ifstream file;
+    string stat;
+    int num;
+    int FGM;            //Field Goals Made
+    int FGA;            //Field Goals Attempted
+    int ThPM;           //Three Points Made
+    int ThPA;           //Three Points Attempted
     
     cout << "Welcome to the LeBron James Museum, please enter the file you wish to analyze: ";
     cin >> fileName;
-    
-    cout << getNumberOfTwoPointsMade(fileName, file);
-    getNumberOfThreePointsMade(fileName, file);
-    getTotalNumberOfShotsAttempted(fileName, file);
-    getPercentageOfTwoPointsMade(fileName, file);
-    getPercentageOfThreePointsMade(fileName, file);
-    IsBetterThanChance(fileName, file);
+    file.open(fileName);
+    while (file >> stat >> num)
+    {
+        if (stat == "FGM") {FGM = num;}         //Gets needed values from file and sets them to internal variables
+        else if (stat == "FGA") {FGA = num;}
+        else if (stat == "3PM") {ThPM = num;}
+        else if (stat == "3PA") {ThPA = num;}
+    }
 
+
+    cout << "Two-pointers made: " << getNumberOfTwoPointsMade(ThPM, FGM) << endl;       //Prints results
+    cout << "Three-pointers made: " << getNumberOfThreePointsMade(ThPM) << endl;
+    cout << "Total number of attempts: " << getTotalNumberOfShotsAttempted(FGA) << endl;
+    cout << "Percent of Two-pointers made: " << getPercentageOfTwoPointsMade(getNumberOfTwoPointsMade(ThPM, FGM), ThPA, FGA) << "%" << endl;
+    cout << "Percent of Three-pointers made: " << getPercentageOfThreePointsMade(ThPM, ThPA) << "%" << endl;
+    if (IsBetterThanChance(FGM, FGA)) {cout << "Lebron has a good chance of scoring" << endl;}
+    else {cout << "Lebron does not have a good chance of scoring" << endl;}
+
+    file.close();
     cin >> fileName;
 }
