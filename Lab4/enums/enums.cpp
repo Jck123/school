1f684d13d50bed08341c917602704328383d2a64
Jck123 <jck.1.2.3.a@gmail.com>
2020-03-12 13:07:09 -0400

    Created cpp folders and files

---
+++ Lab4/enums/enums.cpp
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////
+// Filename: enums.cpp                                                  //
+// Date: March 12, 2020                                                 //
+// Programmer: James Kelly                                              //
+//                                                                      //
+// Description:                                                         //
+//      This program gives you the option between two different         //
+//      functions that you can run and the user decides when to         //
+//      stop running the program                                        //
+//////////////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+enum letter_grade {A, A_PLUS, A_MINUS, B, B_PLUS, B_MINUS, C, C_PLUS, C_MINUS, D, D_PLUS, D_MINUS, F};
+letter_grade deriveGrade(double average);
+
+int main()
+{
+
+}
---
+++ Lab4/strings/strings.cpp
@@ -0,0 +1,10 @@
+//////////////////////////////////////////////////////////////////////////
+// Filename: strings.cpp                                                //
+// Date: March 12, 2020                                                 //
+// Programmer: James Kelly                                              //
+//                                                                      //
+// Description:                                                         //
+//      This program gives you the option between two different         //
+//      functions that you can run and the user decides when to         //
+//      stop running the program                                        //
+//////////////////////////////////////////////////////////////////////////
