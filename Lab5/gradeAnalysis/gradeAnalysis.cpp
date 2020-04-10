c930bfed190b7687fbf77633edc6697ca351fb8c
Jck123 <jck.1.2.3.a@gmail.com>
2020-04-10 17:05:46 -0400

    NO, THERE IS NO ERROR, YOU SHITTY FUCKING LINTER

--- Lab5/gradeAnalysis/gradeAnalysis.cpp
+++ Lab5/gradeAnalysis/gradeAnalysis.cpp
@@ -16,6 +16,8 @@ using namespace std;
 
 int arrStorage(string &fileName)
 {
+    int index = 0;
+    int stuCount = 0;
     double test1;
     double test2;
     double test3;
@@ -24,17 +26,47 @@ int arrStorage(string &fileName)
     string stuName;
     ifstream stuFile;
     stuFile.open(fileName);
+    while (!stuFile.eof())
+    {
+        getline(stuFile, stuName);
+        stuCount++;
+    }
+    stuFile.close();
+    stuFile.open(fileName);
+    string stuNames[stuCount];
+    double stuGrades[stuCount][5];
     while (stuFile >> stuName >> test1 >> test2 >> test3 >> test4 >> test5)
     {
-        
+        stuNames[index] = stuName;
+        stuGrades[index][0] = test1;
+        stuGrades[index][1] = test2;
+        stuGrades[index][2] = test3;
+        stuGrades[index][3] = test4;
+        stuGrades[index][4] = test5;
+        index++;
     }
 }
 
 int main()
 {
-    string fileName;
-    string Names[1];
-    int Grades[1][1];
-    cout << "Welcome to the Student Grade Analysis program, please enter the name of the file you wish to analyze." << endl;
-    cin >> fileName;
+    string fileName = "student_grades.txt";
+    //string Names[1];
+    //int Grades[1][1];
+    //cout << "Welcome to the Student Grade Analysis program, please enter the name of the file you wish to analyze." << endl;
+    //cin >> fileName;
+    arrStorage(fileName);
 }
+
+/*int main()
+{
+    ifstream stuFile;
+    string stuName;
+    int x = 0;
+    stuFile.open("student_grades.txt");
+    while (!stuFile.eof())
+    {
+        getline(stuFile, stuName);
+        x++;
+    }
+    cout << x;
+}*/
