0f1784d1ba832c16f4133a1581b1b293bb2eef53
Jck123 <jck.1.2.3.a@gmail.com>
2020-08-19 18:21:23 -0400

    Added welcometojava

---
+++ CSC-151/Lab_01/welcometojava/src/welcometojava/welcometojava.java
@@ -0,0 +1,9 @@
+package welcometojava;
+
+public class welcometojava {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun.");
+	}
+}
