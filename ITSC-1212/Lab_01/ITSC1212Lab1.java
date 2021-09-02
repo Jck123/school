83c098a4499a260564431da3bfb46eaf0aee13bd
jck <jck.1.2.3.a@gmail.com>
2021-09-02 10:19:17 -0400

    added Lab1

---
+++ ITSC-1212/Lab_01/ITSC1212Lab1.java
@@ -0,0 +1,19 @@
+public class ITSC1212Lab1 {
+    public static void main(String Args[]) {
+        //System.out.print("Hello, ");
+        //System.out.println("World!");
+
+        //Part C
+        int x = 5;
+        System.out.println("The value of x");
+        System.out.println(x);
+        System.out.println("The value of x + 2");
+        System.out.println(x + 2);
+        System.out.println("The value of x * x");
+        System.out.println(x * x);
+        System.out.println("The value of x / 2");
+
+        double y = x / 2.0;
+        System.out.println(y);
+    }
+}
