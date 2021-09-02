511456f9d312e52eb8295df789e0e71118c3d067
jck <jck.1.2.3.a@gmail.com>
2021-09-02 10:39:20 -0400

    Completed Lab01

--- ITSC-1212/Lab_01/ITSC1212Lab1.java
+++ ITSC-1212/Lab_01/ITSC1212Lab1.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class ITSC1212Lab1 {
     public static void main(String Args[]) {
         //System.out.print("Hello, ");
         //System.out.println("World!");
 
         //Part C
-        int x = 5;
+        /*int x = 5;
         System.out.println("The value of x");
         System.out.println(x);
         System.out.println("The value of x + 2");
@@ -14,6 +16,15 @@ public class ITSC1212Lab1 {
         System.out.println("The value of x / 2");
 
         double y = x / 2.0;
-        System.out.println(y);
+        System.out.println(y);*/
+
+        //Part E
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a temperature in F.");
+        double tempF = input.nextDouble();
+        System.out.println("The value entered was " + tempF);
+        double tempC = (tempF - 32) * (5.0 / 9.0);
+        System.out.println("The temperature in C is: " + tempC);
     }
 }
