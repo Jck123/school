8128e49cfa3aa37f9cab39f88bb6ac071a9979d3
Jck123 <jck.1.2.3.a@gmail.com>
2023-01-12 17:48:45 -0500

    complete exercise 1

--- ITSC-3155/Intro_to_python/exercise_01.py
+++ ITSC-3155/Intro_to_python/exercise_01.py
@@ -1,1 +1,13 @@
-print("This line will be printed.")
+grade = int(input("Please enter your number grade: "))
+
+if not(grade >= 0 and grade <= 100):
+    print("Invalid number! Please try again...")
+    exit()
+
+if grade >= 90: letGrade = 'A'
+elif grade >= 80: letGrade = 'B'
+elif grade >= 70: letGrade = 'C'
+elif grade >= 60: letGrade = 'D'
+else: letGrade = 'F'
+
+print("Your grade is " + letGrade)
