cab921301b592920be5b59f7e797038e230ce237
Jck123 <jck.1.2.3.a@gmail.com>
2023-01-12 18:40:06 -0500

    completed exercise 4

---
+++ ITSC-3155/Intro_to_python/exercise_04.py
@@ -0,0 +1,9 @@
+listNum = int(input("Enter a number: "))
+
+numList = list()
+
+for i in range(listNum):
+    numList.append(float(input("Enter number " + str(i + 1) + ":")))
+
+print("List: " + str(numList))
+print("Average: ", round(sum(numList) / len(numList), 3))
