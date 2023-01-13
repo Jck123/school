9116fc39be0c851cbca9026af18c1285047841d6
Jck123 <jck.1.2.3.a@gmail.com>
2023-01-13 18:39:54 -0500

    completed exercise 9

---
+++ ITSC-3155/Intro_to_python/exercise_09.py
@@ -0,0 +1,11 @@
+wordList = list()
+
+for i in range(5):
+    wordList.append(input("Enter a word: "))
+
+print("Words:", wordList)
+
+print("One String: ", end = "")
+for s in wordList:
+    print(s, end = " ")
+print()
