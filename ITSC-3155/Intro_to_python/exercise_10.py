311b6c5d1742cf8092e7000acacc7d4ef85b80ab
Jck123 <jck.1.2.3.a@gmail.com>
2023-01-13 18:52:16 -0500

    Completed exercise 10

---
+++ ITSC-3155/Intro_to_python/exercise_10.py
@@ -0,0 +1,15 @@
+strIn = input("Enter a string: ")
+
+bigList = list()
+smallList = list()
+
+for s in strIn:
+    smallList.append(s)
+    if len(smallList) == 3:
+        bigList.append(smallList.copy())
+        smallList.clear()
+else:
+    if len(smallList) > 0:
+        bigList.append(smallList.copy())
+
+print(bigList)
